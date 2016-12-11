294ec1a2698e84f8f666d9a3af9f14a890045b26
creuter <christian.reuter@kom.tu-darmstadt.de>
2016-12-11 16:27:26 +0100

    Fixed kitchen objects not positioning their colliders.

--- Sources/KitchenObject.cpp
+++ Sources/KitchenObject.cpp
@@ -1,18 +1,27 @@
 #include "KitchenObject.h"
 #include <Kore/Math/Quaternion.h>
 
+namespace {
+	void setM(MeshObject* mesh, mat4 M) {
+		if (mesh != nullptr) {
+			mesh->M = M;
+			for (int k = 0; k < mesh->colliderCount; ++k) {
+				float distance;
+				if (mesh->collider[k] != nullptr) {
+					mesh->collider[k]->setPos(M * vec4(0, 0, 0, 1));
+				}
+			}
+		}
+	}
+}
+
 KitchenObject::KitchenObject(MeshObject* body, MeshObject* door, vec3 position, vec3 rotation, vec3 offset) : body(body), door(door), position(position), rotation(rotation), closed(true), offset(offset) {
     
     mat4 M = mat4::Translation(position.x(), position.y(), position.z());
     M *= mat4::Rotation(rotation.x(), rotation.y(), rotation.z());
-    
-    if (body != nullptr) {
-        body->M = M;
-    }
-    if (door != nullptr) {
-        door->M = M;
-    }
 
+	setM(body, M);
+	setM(door, M);
 }
 
 void KitchenObject::render(TextureUnit tex, ConstantLocation mLocation) {
